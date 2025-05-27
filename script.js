3fe3c62780399e2835238b1e88d7d98b9d086797
topmost003 <topmostdabest@gmail.com>
2025-05-27 19:32:09 +0100

    practice

---
+++ script.js
@@ -0,0 +1,20 @@
+  
+
+// let greet = (Button = greeting)
+
+// function changeButton(){ 
+//  let greeting = document.getElementById(`hello`)
+// let Button =   document.getElementById(`change`)
+// // let Button2 = "thanks for coming"
+
+// if (greeting.innerText === "welcome back")
+//    {
+//      greeting.innerText =`welcome back`}
+   
+// else
+// {
+//      greeting.innerText = `THANKS FOR COMiiING`
+
+// }
+ 
+// }
