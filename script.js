ccc6f5fc0eb8a9279c037669fae8d3aaea34e038
topmost003 <topmostdabest@gmail.com>
2025-05-27 23:34:00 +0100

    update

--- script.js
+++ script.js
@@ -18,3 +18,37 @@
 // }
  
 // }
+//  let box = document.getElementById (`box`)
+//     var animation;
+
+// function epicMovement(){
+   
+//   movementAmount += 5,
+//   box.style.left = movementAmount  + `px`;
+//   console.log (box.style.left);
+
+// animation = setTimeout (epicMovement,20);
+// }
+
+//  epicMovement()
+
+
+
+// function checkEmail(email) {
+//     if(email.includes("@gmail.com")){
+//         console.log( "true")
+//     } 
+//     else {console.log(false)}
+
+// }
+
+// checkEmail("topmost@gmail.com")
+
+
+
+let myName = "fadipe quadri abiodun"
+let age = 10
+
+document.getElementById ("user").textContent = `my name is ${myName}`
+document.getElementById ("userAge").textContent =  age
+
