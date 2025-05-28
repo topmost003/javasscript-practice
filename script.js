b29035118377ff52244855e4802bce8606b1f98e
topmost003 <topmostdabest@gmail.com>
2025-05-28 13:06:28 +0100

    update

--- script.js
+++ script.js
@@ -72,15 +72,48 @@
 //  document.getElementById(`input1`)
  
 
-function userName(){
-  user = document.getElementById(`input`).value,
-  console.log(user)
+// function userName(){
+//   user = document.getElementById(`input`).value,
+//   console.log(user)
 
-  password = document.getElementById(`password`).value,
-  console.log(password.includes(`@`))
+//   password = document.getElementById(`password`).value,
+//   console.log(password.includes(`@`))
 
-  document.getElementById(`myName`).innerText = (`welcome ${user}`)
+//   document.getElementById(`myName`).innerText = (`welcome ${user}`)
 
-}
+// }
+
+
+// TYPE OF
+
+// let a = 10
+// console.log ( typeof a)
+
+// let b = "10"
+// b = Number(b)
+// console.log ( typeof b)
+
+
+// CONST (calculating a circumference)
+
+// const pi = 22/7
+// const radius =  parseFloat(prompt(`enter the radius`))
+// circumference = pi * radius * 2
+
+// console.log(circumference)
+
+const pi = 3.142
+let circumference;
+let radius;
+
+function calculate(){
+let radius = document.getElementById (`input`).value;
+radius = Number(radius);
+let circumference = 2 * pi * radius;
+console.log(circumference);
+
+document.getElementById (`answer`).innerText = circumference
+
+} 
 
 
